acaec903335983ba33eef697b5bd08852f298b5b
Gustavo <gu30castro@gmail.com>
2026-03-12 21:38:30 -0300

    Continuando a aula do comando For

--- Secao 1_Logica de programacao/For.py/For_nested+exer.py
+++ Secao 1_Logica de programacao/For.py/For_nested+exer.py
@@ -47,3 +47,59 @@ print(quadrados_impar) : Executa o valor da lista definido de acordo com o proce
 
 
 """
+
+print()
+
+# compreensão de lista 2 
+
+quadrados_impa = []
+
+for x in range (1, 11):
+
+    if x % 2 != 0:
+
+        quadrados_impa.append(x**2)
+
+print(f"{quadrados_impa}")
+
+print()
+
+#Exercicios com caracteres cosoantes 
+texto = "Hello World"
+
+consoantes = [char for char in texto if char.lower() not in "aeiou"]
+print(consoantes)
+
+print()
+
+# consoantes = [char for char in texto if char.lower() not in "aeiou"]
+
+exemplo_2 = []      # lista vazia para receber os caracteres
+
+for letra in texto: # loop para verificar cada letra na variavel texto
+
+    if letra.lower() not in "aeiou": # transforma os char em minusculo e verifica se é vogal
+
+        exemplo_2.append(letra) #adiciona as consoantes na lista vazia 
+
+print(exemplo_2) # mostra o resultado na tela.
+
+print()
+
+# EXERCICIO 
+
+"""
+Crie um programa em python que solicite um numero inteiro positivo e calcule o fatorial do msm.
+"""
+
+fatorial = int(input("Digite um numero inteiro positivo: "))
+
+multi = 1
+
+for i in range(1, fatorial + 1):
+    
+    multi = multi * i 
+
+print (f" O fatorial de {fatorial} é {multi}")
+
+  
