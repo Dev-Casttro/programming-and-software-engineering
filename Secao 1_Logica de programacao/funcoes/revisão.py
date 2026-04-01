ba8503b70f8fb7a40901d0ac4f219099f947edcc
Gustavo <gu30castro@gmail.com>
2026-04-01 20:26:10 -0300

    Study: prática de cadastro de cliente com função e simulação de entrada de dados(continuação da aula de funçôes)

--- Secao 1_Logica de programacao/funcoes/revisão.py
+++ Secao 1_Logica de programacao/funcoes/revisão.py
@@ -34,3 +34,26 @@ N2 = int(input("digite um número: "))
 
 resultado = soma(N1, N2)
 print(f"O resultado da soma é: {resultado}")
+
+
+print()
+
+# Pratica 3 
+
+while True:
+
+   def dados_cliente(nome, args_idade, args_data_nascimento):
+
+      print(f"\nDados do cliente:\nNome: {nome}\nIdade: {args_idade}\nNascimento: {args_data_nascimento}")
+      
+      print(args_idade + args_data_nascimento)
+
+      return nome, *args_idade, *args_data_nascimento
+
+   nome_cliente = input("Nome: ")
+   idade_cliente = input("Idade: ")
+   data_nasc_cliente = input("Data de nascimento: ")
+
+   dados_cliente(nome_cliente, idade_cliente, data_nasc_cliente)
+
+   print("Cadastro concluido com sucesso!")
